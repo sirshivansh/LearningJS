be8b01fa0120ce00835d388acd7ae4676c2c5a88
Shivansh Mishra <shivnsh01@gmail.com>
2025-06-13 07:24:02 +0530

    Variables in JS

---
+++ basic01.js
@@ -0,0 +1,11 @@
+var num1 = 2;
+var num2 = 4;
+var num3 = 6;
+
+var sum = num1+num2+num3;
+console.log(sum);
+
+var firstName = "Shivansh";
+var lastName = 'Mishra';
+console.log(firstName);
+console.log(lastName);
