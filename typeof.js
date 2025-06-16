71b414717ad089bdb2a77498e6cdb7f47d75a598
Shivansh Mishra <shivnsh01@gmail.com>
2025-06-16 22:28:28 +0530

    Exploring 'typeof' operator

---
+++ typeof.js
@@ -0,0 +1,6 @@
+console.log(typeof 2); //number
+console.log(typeof 'hello'); //string
+console.log(typeof undefined); //undefined
+console.log(typeof {}); //object
+console.log(typeof []); //object
+console.log(typeof ((x) => x*2)); //function
